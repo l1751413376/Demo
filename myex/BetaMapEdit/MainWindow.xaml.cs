5b1a6699ff2fa9b7e4a29228d5eee9d47b8d74a7
刘毅 <ly09614@ly.com>
2016-05-17 17:47:15 +0800

    1

--- myex/BetaMapEdit/MainWindow.xaml.cs
+++ myex/BetaMapEdit/MainWindow.xaml.cs
@@ -22,11 +22,12 @@ namespace BetaMapEdit
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int GridSize = 20; //单位格子大小
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        SolidColorBrush redsolid = new SolidColorBrush(Colors.Red);
         private void button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog loadMap = new OpenFileDialog()
@@ -41,15 +42,73 @@ namespace BetaMapEdit
                 Filter = "图像文件(*.jpg,*.png)|*.jpg;*.png",
 
             };
-
-            loadMap.FileOk += new System.ComponentModel.CancelEventHandler(loadMap_FileOk);
-
+            
+            loadMap.FileOk += new CancelEventHandler(loadMap_FileOk);
+            
             loadMap.ShowDialog();
         }
 
         public void loadMap_FileOk(object sender, CancelEventArgs e)
         {
-            
+            OpenFileDialog dialog = sender as OpenFileDialog;
+            var file=dialog.OpenFile();
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.StreamSource = file;
+            img.EndInit();
+
+
+            Map.Background = new ImageBrush(img);
+            Map.Height = img.Height;
+            Map.Width = img.Width;
+
+            for (var x = 0; x < img.Width; x += GridSize)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Width = 1;
+                rect.Height = img.Height;
+                rect.Fill = new SolidColorBrush(Colors.Red);
+                Canvas.SetLeft(rect, x);
+                Canvas.SetTop(rect, 0);
+                Canvas.SetZIndex(rect, 100);
+                GridCarrier.Children.Add(rect);
+            }
+
+            for (var x = 0; x < img.Height; x += GridSize)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Width = img.Width;
+                rect.Height = 1;
+                rect.Fill = new SolidColorBrush(Colors.Red);
+                Canvas.SetLeft(rect, 0);
+                Canvas.SetTop(rect, x);
+                Canvas.SetZIndex(rect, 100);
+                GridCarrier.Children.Add(rect);
+            }
+        }
+
+        private void GridCarrier_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(Map);
+            p = new Point()
+            {
+                X = p.X / (GridSize),
+                Y = p.Y / (GridSize)
+            };
+            Rectangle rect = new Rectangle();
+
+            Canvas.SetLeft(rect, (int)p.X * GridSize);
+            Canvas.SetTop(rect, (int)p.Y * GridSize);
+            rect.Width = GridSize;
+            rect.Height = GridSize;
+            rect.Fill = redsolid;
+            Canvas.SetZIndex(rect, 102);
+            rect.MouseLeftButtonDown += (s1,e1) => 
+            {
+                Map.Children.Remove((Rectangle)s1);
+                e.Handled = true;
+            };
+            Map.Children.Add(rect);
         }
     }
 }

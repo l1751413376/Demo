282b8d4c4bafe64449c40fc32a0d7ebb75eac1b9
刘毅 <ly09614@ly.com>
2016-05-20 18:42:40 +0800

    1

--- myex/CsTest/Program.cs
+++ myex/CsTest/Program.cs
@@ -11,120 +11,97 @@ namespace CsTest
 {
     static class Program
     {
+       
+           
+        public static Regex RegProtocol = new Regex("(GET|POST)|([^ ]+)");
+        public static Regex RegItem = new Regex("([^ :]+)");
         static void Main(string[] args)
         {
-            var file = File.Open(@"D:\MyConfiguration\ly09614.TCENT\Desktop\Unity3D5.0\破解补丁\1222.map", FileMode.Open);
-            MapInfo map = new MapInfo();
-            map.Flag = (int)file.ReadDword();
-            map.Width = file.ReadDword();
-            map.Height = file.ReadDword();
-            int m_MapBlockRowNum = (int)Math.Ceiling(map.Height / (double)240);
-            int m_MapBlockColNum = (int)Math.Ceiling(map.Width / (double)320);
+            var str = @"POST http://ebk.t.17u.cn/ivacation/VisaInfo/UploadETA?q=1&PassengerId=3891314&PassengerName=%E6%B5%8B%E8%AF%95&CustomerSerialId=&OrderSerialid=undefined HTTP/1.1
+Host: ebk.t.17u.cn
+Connection: keep - alive
+Content - Length: 846125
+Pragma: no - cache
+Cache - Control: no - cache
+Accept: */*
+Origin: http://ebk.t.17u.cn
+X-Requested-With: XMLHttpRequest
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryySzFkyO1blOdPmrr
+Referer: http://ebk.t.17u.cn/ivacation/VisaInfo/VisaOrderList
+Accept-Encoding: gzip,deflate
+Accept-Language: zh-CN,zh;q=0.8
+Cookie: ASP.NET_SessionId=vqbklzbaukniy32ehd3jmti1; Hm_lvt_efeba49561e17cb591fcf93d3a48f843=1463718412; Hm_lpvt_efeba49561e17cb591fcf93d3a48f843=1463718412
 
-            //----------------------------------
-            // 读偏移信息
-            //----------------------------------
-            int block_num = m_MapBlockRowNum * m_MapBlockColNum;
-            uint[] pBlockOffset = new uint[block_num];
-            for (var i = 0; i < pBlockOffset.Length; i++)
-            {
-                pBlockOffset[i] = file.ReadDword();
-            }
+------WebKitFormBoundaryySzFkyO1blOdPmrr
+";
 
-            //块
-            SBlockInfo[] blocks = new SBlockInfo[block_num];
+            var matches = RegItem.Matches(str);
 
-            for (int i = 0; i < block_num; i++)
-            {
-                var item = blocks[i]=new SBlockInfo();
-                file.Seek(pBlockOffset[i], SeekOrigin.Begin);
-                uint usize = file.ReadDword();
-                file.Seek(usize, SeekOrigin.Current);
-                var flag = file.ReadDword();
-
-                //var flag2 = file.ReadDword();
-            }
-
-
-            //mask
-            map.MaskFlag = file.ReadDword();
-            map.MaskNum = (int)file.ReadDword();
-
-            uint[] pMaskDataOffset = new uint[map.MaskNum];
-
-            for (var i = 0; i < map.MaskNum; i++)
-            {
-                pMaskDataOffset[i] = file.ReadDword();
-            }
-            map.MaskInfos = new MaskInfo[map.MaskNum];
-            for (var i = 0; i < map.MaskNum; i++)
-            {
-                file.Seek(pMaskDataOffset[i], SeekOrigin.Begin);
-                var item = map.MaskInfos[i] = new MaskInfo();
-                item.startX = (int)file.ReadDword();
-                item.startY = (int)file.ReadDword();
-                item.width = (int)file.ReadDword();
-                item.height = (int)file.ReadDword();
-                item.mask_size = (int)file.ReadDword();
-
-                byte[] pMaskDataOrgin = new byte[item.mask_size];
-                file.Read(pMaskDataOrgin, 0, item.mask_size);
-                //解压mask
-                int align_width = (item.width / 4 + ((item.width % 4 != 0) ? 1 : 0)) * 4;	// 以4对齐的宽度
-                byte[] pMaskDataDec = new byte[align_width * item.height / 4];
-
-
-                
-            }
-
-            
+            _HttpRequest request = new _HttpRequest();
+            request.ParseFromByte(Encoding.UTF8.GetBytes(str));
         }
-        
-       
+
     }
 
-    public static class FileEx
+    public class _HttpRequest
     {
-        public static uint ReadDword(this FileStream stream)
+        public Dictionary<string, string> Parameter = new Dictionary<string, string>();
+        private int Cols;
+
+        #region Reg
+        public static Regex RegProtocol = new Regex("(GET|POST)|[^ ]+");
+        public static Regex RegItem = new Regex("([^ :]+)");
+
+        #endregion
+        public void ParseFromByte(byte[] buffer)
         {
-            byte[] b = new byte[4];
-            stream.Read(b, 0, 4);
-            return BitConverter.ToUInt32(b, 0);
+            Cols = 0;
+            MemoryStream stream = new MemoryStream(buffer);
+            HeaderProcess(ReadCol(stream));
+            string content;
+            while ((content = ReadCol(stream)) != null)
+            {
+                ItemProcess(content);
+            }
+
+        }
+        public string ReadCol(MemoryStream stream)
+        {
+            var p = stream.Position;
+            var count = 0;
+            while (true)
+            {
+                if (stream.ReadByte() == 0x0D && stream.ReadByte() == 0x0A)
+                {
+                    break;
+                }
+                count++;
+            }
+            if (count == 0)
+            {
+                return null;//head 结束
+            }
+            stream.Seek(p, SeekOrigin.Begin);
+            count += 2;
+            var content = new byte[count];
+            stream.Read(content, 0, count);
+            var str = Encoding.UTF8.GetString(content);
+            return str;
+        }
+        public void HeaderProcess(string content)
+        {
+            var matches = RegProtocol.Matches(content);
+            Parameter.Add("Method", matches[0].Value);
+            Parameter.Add("Url", matches[1].Value);
+            Parameter.Add("Protocol", matches[2].Value);
+        }
+        public void ItemProcess(string content)
+        {
+            var Match = RegItem.Match(content);
+            Parameter.Add(Match.Value, content.Replace(Match.Value+": ", ""));
+
         }
     }
 
-    public class MapInfo
-    {
-        public int Flag;
-        public uint Width;
-        public uint Height;
-        public uint MaskFlag;
-        /// <summary>
-        /// mask数量
-        /// </summary>
-        public int MaskNum;
-        public MaskInfo[] MaskInfos;
-        /// <summary>
-        /// 图像
-        /// </summary>
-        public byte[] MapImg;
-
-    }
-
-    public class MaskInfo
-    {
-        public int startX;
-        public int startY;
-        public int width;
-        public int height;
-        public int mask_size;
-        public byte[] img;
-    }
-
-    public class SBlockInfo
-    {
-        public uint UnitN;	// 未知数据大小
-        public uint flag;
-        public uint size;
-    };
 }
--- myex/WebService/MainWindow.xaml.cs
+++ myex/WebService/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,7 @@ namespace WebService
     /// </summary>
     public partial class MainWindow : Window
     {
+        const char c = '\r';
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +35,7 @@ namespace WebService
         private void button_Click(object sender, RoutedEventArgs e)
         {
             var btn = ((Button)sender);
-            
+
             if (btn.Content.ToString() == "启动")
             {
                 //创建IP地址
@@ -80,7 +83,6 @@ namespace WebService
 
         public void ProcessRequestSocket(Socket s)
         {
-            HttpWebResponse re = new HttpWebResponse(;
             var response = @"
 HTTP/1.1
 Cache-Control: private
@@ -89,13 +91,73 @@ Vary: Accept-Encoding
 Content-Length: 10
 
 HelloWorld";
-
             Byte[] bytesSent = Encoding.ASCII.GetBytes(response);
             Byte[] bytesReceived = new Byte[655350];
             s.Receive(bytesReceived, bytesReceived.Length, 0);
             var request = Encoding.ASCII.GetString(bytesReceived);
             s.Send(bytesSent, bytesSent.Length, 0);
             s.Close();
+        }
+
+
+    }
+    public class _HttpRequest
+    {
+        public Dictionary<string, string> Parameter = new Dictionary<string, string>();
+        private int Cols;
+
+        #region Reg
+        public static Regex RegProtocol = new Regex("(GET|POST)|[^ ]+");
+        public static Regex RegItem = new Regex("([^ :]+)");
+
+        #endregion
+        public void ParseFromByte(byte[] buffer)
+        {
+            Cols = 0;
+            MemoryStream stream = new MemoryStream(buffer);
+            HeaderProcess(ReadCol(stream));
+            string content;
+            while ((content= ReadCol(stream))!=null)
+            {
+                ItemProcess(content);
+            }
+
+        }
+        public string ReadCol(MemoryStream stream)
+        {
+            var p = stream.Position;
+            var count = 0;
+            while (true)
+            {
+                if (stream.ReadByte() == 0x0D && stream.ReadByte() == 0x0A)
+                {
+                    break;
+                }
+                count++;
+            }
+            if (count == 0)
+            {
+                return null;//head 结束
+            }
+            stream.Seek(p, SeekOrigin.Begin);
+            count += 2;
+            var content = new byte[count];
+            stream.Read(content, 0, count);
+            var str = Encoding.UTF8.GetString(content);
+            return str;
+        }
+        public void HeaderProcess(string content)
+        {
+            var matches=RegProtocol.Matches(content);
+            Parameter.Add("Method", matches[0].Value);
+            Parameter.Add("Url", matches[1].Value);
+            Parameter.Add("Protocol", matches[2].Value);
+        }
+        public void ItemProcess(string content)
+        {
+            var matches = RegItem.Matches(content);
+            Parameter.Add(matches[0].Value, matches[1].Value);
+
         }
     }
 }
